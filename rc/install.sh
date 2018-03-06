bdf14cb67119ab3690cfc413590145e7e8c4231e
Vedant Kumar <vsk@apple.com>
2018-03-06 12:18:00 -0800

    Set up ctags reindexing via git hooks

--- rc/install.sh
+++ rc/install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cp vimrc ~/.vimrc
+
 echo "source ~/scripts/rc/zshrc" > ~/.zshrc
+
 cp tmuxrc ~/.tmux.conf
+
 cp gitconfig ~/.gitconfig
+
+# Run ctags when setting up or updating repos.
+git config --global init.templatedir '~/.git_template'
+cp ctags-reindex ~/.git_template/hooks/ctags
+mkdir -p ~/.git_template/hooks
+cp run-ctags-reindex ~/.git_template/hooks/post-rewrite
+cp run-ctags-reindex ~/.git_template/hooks/post-checkout
