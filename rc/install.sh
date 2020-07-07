1948fecc38ca2f54c29d2440fbc60bcded5cf2ac
Vedant Kumar <vsk@apple.com>
2020-07-07 16:10:49 -0700

    Hook up lldb.sh

--- rc/install.sh
+++ rc/install.sh
@@ -8,10 +8,11 @@ cp tmuxrc ~/.tmux.conf
 
 cp gitconfig ~/.gitconfig
 
+cp lldbinit ~/.lldbinit
+
 # Run ctags when setting up or updating repos.
-mkdir -p ~/.git_template
+mkdir -p ~/.git_template/hooks
 git config --global init.templatedir '~/.git_template'
 cp ctags-reindex ~/.git_template/hooks/ctags
-mkdir -p ~/.git_template/hooks
 cp run-ctags-reindex ~/.git_template/hooks/post-rewrite && chmod +x ~/.git_template/hooks/post-rewrite
 cp run-ctags-reindex ~/.git_template/hooks/post-checkout && chmod +x ~/.git_template/hooks/post-checkout
